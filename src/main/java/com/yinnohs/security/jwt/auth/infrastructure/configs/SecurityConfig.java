1408a24e03258246df40d6c56aef8997464984a3
yinnohs (jose ignacio soto leon) <joistleon@gmail.com>
2025-03-31 23:10:53 +0100

    Feature :sparkles: (auth): updated security configuration

---
+++ src/main/java/com/yinnohs/security/jwt/auth/infrastructure/configs/SecurityConfig.java
@@ -0,0 +1,51 @@
+package com.yinnohs.security.jwt.auth.infrastructure.configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Value("${argon2.memory}")
+    private int memory;
+
+    @Value("${argon2.iterations}")
+    private int iterations;
+
+    @Value("${argon2.parallelism}")
+    private int parallelism;
+
+    @Value("${argon2.saltLength}")
+    private int saltLength;
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request->{
+                    request.anyRequest().permitAll();
+                });
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        int hashLength = 128;
+        return new Argon2PasswordEncoder(
+                saltLength,
+                hashLength,
+                parallelism,
+                memory,
+                iterations
+        );
+    }
+}
